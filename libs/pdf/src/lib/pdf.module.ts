98414f59caa45607e97b9cfb095b5248568373a1
yonatanaltman <yonatan@yaltman.com>
2023-02-02 15:45:20 +0200

    with heavy libs

--- libs/pdf/src/lib/pdf.module.ts
+++ libs/pdf/src/lib/pdf.module.ts
@@ -1,10 +1,11 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { PdfComponent } from './pdf/pdf.component';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {PdfComponent} from './pdf/pdf.component';
 import {RouterModule} from "@angular/router";
 
 @NgModule({
-  imports: [CommonModule,RouterModule.forChild([{path:'',component:PdfComponent}])],
-  declarations: [PdfComponent],
+    imports: [CommonModule, RouterModule.forChild([{path: '', component: PdfComponent}])],
+    declarations: [PdfComponent],
 })
-export class PdfModule {}
+export class PdfModule {
+}
--- libs/pdf/src/lib/pdf/pdf.component.ts
+++ libs/pdf/src/lib/pdf/pdf.component.ts
@@ -1,8 +1,18 @@
-import { Component } from '@angular/core';
+import {ChangeDetectionStrategy, Component} from '@angular/core';
+import {jsPDF} from "jspdf";
 
 @Component({
-  selector: 'super-lazy-pdf',
-  templateUrl: './pdf.component.html',
-  styleUrls: ['./pdf.component.scss'],
+    selector: 'super-lazy-pdf',
+    templateUrl: './pdf.component.html',
+    styleUrls: ['./pdf.component.scss'],
+    changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class PdfComponent {}
+export class PdfComponent {
+    exportToPdf() {
+// Default export is a4 paper, portrait, using millimeters for units
+        const doc = new jsPDF();
+
+        doc.text("Hello world!", 10, 10);
+        doc.save("a4.pdf");
+    }
+}
